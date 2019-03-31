32b539faf21f07b5b9b3281e4b76c2518085fae1
Chance Hudson <jchancehud@gmail.com>
2019-03-31 05:42:52 -0500

    Call next automatically

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 module.exports = (handler) => {
-  // args are [req, res, next], the catch function pops the last argument and
-  // gives it whatever error is received (the equivalent of next(err)).
-  //
-  // The pop operation modifies the array so it's wrapped in an arrow function
-  // instead of being passed directly (if we get to the error handler we can 
-  // manipulate args as it's done being used by the handler).
-  return (...args) => Promise.resolve(handler(...args)).catch((err) => args.pop()(err));
+  return (...args) => {
+    let nextCalled = false;
+    const next = args.pop();
+    const _next = () => {
+      if (nextCalled) return;
+      nextCalled = true;
+      next();
+    };
+    args.push(_next);
+    return Promise.resolve(handler(...args))
+      .then(() => next())
+      .catch(_next)
+  }
 };
